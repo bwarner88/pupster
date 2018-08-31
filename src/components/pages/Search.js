071938a61f50ee9ec25d5879d7276233f0e339b0
unknown <brian_warner25@yahoo.com>
2018-08-30 18:17:54 -0700

    got search page to show

---
+++ src/components/pages/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Search = () => (
+    <div>
+        <form className = "search"></form>
+        <button className = "submit"></button>
+    </div>
+)
+
+export default Search;
